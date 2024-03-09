d172cf9db640079b39b6da087fef792989764924
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2024-03-09 22:12:25 +0100

    Add shadcn

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,13 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 import { useEffect, useState } from "react";
 
 export function useWasMounted() {
   const [wasMounted, setWasMounted] = useState(false);
   useEffect(() => setWasMounted(true), []);
   return wasMounted;
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
