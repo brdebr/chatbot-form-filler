0c9d784a08968f48953dc05e5f0c1b6cd9d2e1b0
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2024-03-10 17:46:13 +0100

    Chat progress

--- app/style-constants.ts
+++ app/style-constants.ts
@@ -4,18 +4,18 @@ const theme_styles = /*ui*/ {
   bg_default: `
   bg-zinc-50 dark:bg-slate-900
     transition-colors duration-700
-  `,
+  ` as const,
   card_bg_color: `
     bg-white dark:bg-slate-950
     transition-colors duration-500
-  `,
+  ` as const,
   default_text_color: `
     text-black dark:text-white
     transition-colors duration-[450ms]
-  `,
+  ` as const,
   default_text_size: `
     text-sm sm:text-base
-  `,
+  ` as const,
 }
 
 export { theme_styles }
