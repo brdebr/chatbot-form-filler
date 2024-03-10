b7219e4c3d4ace7d9338775a23d766c746f1a2ca
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2024-03-10 16:15:12 +0100

    Add start of chat component

--- app/components/chat-trigger-button.tsx
+++ app/components/chat-trigger-button.tsx
@@ -1,18 +1,73 @@
 'use client';
 import { Button } from "@/components/ui/button";
-import { ChatBubbleIcon } from '@radix-ui/react-icons';
+import { ChatBubbleIcon, Cross2Icon } from '@radix-ui/react-icons';
 import { cn } from "@/lib/utils";
 import { useWasMounted } from "../hooks/utils";
 import { LoadingIcon } from "./utils/loading-icon";
+import { useState } from "react";
+import { TypographyH4 } from "./basic/h4";
+import { theme_styles } from "../style-constants";
 
 export function ChatTriggerButton(props: {
   onClick?: () => void;
 }) {
   const wasMounted = useWasMounted();
+  const [isChatOpen, setIsChatOpen] = useState(false);
+
+  const openChat = () => {
+    setIsChatOpen(true);
+  }
+
+  if (isChatOpen) {
+    return (
+      <div className={cn(`
+        rounded-md
+        fixed bottom-4 right-4
+        max-h-[80dvh] max-w-[80dvw]
+        w-[450px] h-[550px]
+        bg-violet-800
+        dark:bg-purple-900
+        text-white
+        p-2
+        flex flex-col
+      `)}>
+        <div className={cn(`
+          flex justify-between items-center
+          pl-3
+        `)}>
+          <TypographyH4 className="dark">
+            Chat with the bot
+          </TypographyH4>
+          <Button
+            onClick={() => setIsChatOpen(false)}
+            variant='outline'
+            size='icon'
+            className={cn(`
+              bg-violet-800 hover:bg-violet-600
+              dark:bg-purple-900 dark:hover:bg-purple-700
+              text-white hover:text-white
+              p-2
+            `)}
+          >
+            <Cross2Icon fontSize={23} />
+          </Button>
+        </div>
+        <div className={cn(`
+            h-full
+            rounded
+            ${theme_styles.bg_default}
+            mt-3
+            p-2
+        `)}>
+          
+        </div>
+      </div>
+    )
+  }
 
   return (
     <Button
-      onClick={props.onClick}
+      onClick={openChat}
       variant='outline'
       size='icon'
       className={cn(`
