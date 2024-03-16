dfb5bd49d6474008a158b05f681279811afa3a00
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2024-03-16 23:05:50 +0100

    Improve Chat

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -2,13 +2,13 @@ import { cn } from "@/lib/utils";
 import { theme_styles } from "../style-constants";
 import { Button } from "@/components/ui/button";
 import { PaperPlaneIcon } from "@radix-ui/react-icons";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { Input } from "@/components/ui/input";
 
 type ChatMessageProps = {
   side: 'left' | 'right';
-  message: string;
+  message: ChatMessageType;
 };
 
 export function ChatMessage({ message, side }: ChatMessageProps) {
@@ -19,6 +19,7 @@ export function ChatMessage({ message, side }: ChatMessageProps) {
           ${side === 'left' ? 'bg-blue-400 bg-opacity-40 dark:bg-blue-900 dark:bg-opacity-50': 'bg-teal-400 bg-opacity-50 dark:bg-teal-800 dark:bg-opacity-70'}
           transition-colors
           w-3/4
+          whitespace-pre-line
           ${side === 'left' ? 'mr-auto' : 'ml-auto'}
           ${theme_styles.default_text_color}
           ${theme_styles.default_text_size}
@@ -26,28 +27,52 @@ export function ChatMessage({ message, side }: ChatMessageProps) {
           p-2
           rounded
       `)}
-    >
-      {message}
-    </div>
+    >{message.content}</div>
   );
 }
 
-const messages = [
-  'Hello',
-  'How are you?',
-  'I am fine, thank you.',
-  'How can I help you?',
-  'I need help with a form.',
-  'Sure, I can help you with that.',
-  '👹 Lorem impsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  '🧙‍♂️ Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.',
+export type ChatMessageType = {
+  content: string;
+  role: 'user' | 'assistant';
+};
+
+const messagesMock: ChatMessageType[] = [
+  { content: 'Hello, How are you?', role: 'user' },
+  { content: 'I am fine, thank you. \n How can I help you?', role: 'assistant' },
+  { content: 'I need help with a form.', role: 'user' },
+  { content: 'Sure, I can help you with that.', role: 'assistant' },
+  { content: '👹 Lorem impsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', role: 'user' },
+  { content: '🧙‍♂️ Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.', role: 'assistant' },
 ]
+
 export const Chat = React.forwardRef<HTMLDivElement, {}>((props, ref) => {
+  const [messages, setMessages] = useState<ChatMessageType[]>([]);
   const [chatInput, setChatInput] = useState('');
 
   const handleChatInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setChatInput(e.target.value);
   }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (chatInput.trim() === '') return;
+
+    setMessages([...messages, {
+      content: chatInput,
+      role: 'user',
+    }]);
+    setChatInput('');
+    inputRef.current?.blur();
+  }
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (!messagesContainerRef.current) {
+      return;
+    }
+    messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
+  }, [messages]);
+  const messagesContainerRef = React.useRef<HTMLDivElement>(null);
 
   return (
     <div
@@ -65,11 +90,13 @@ export const Chat = React.forwardRef<HTMLDivElement, {}>((props, ref) => {
         gap-3
     `)}>
       <div
+        ref={messagesContainerRef}
         className={cn(`
-          h-[calc(100%-45px)]
+          h-[calc(100%-60px)]
           max-h-full
           -mr-1
           px-1
+          pb-2
           overflow-y-scroll
           rounded
           ${theme_styles.bg_default}
@@ -77,15 +104,17 @@ export const Chat = React.forwardRef<HTMLDivElement, {}>((props, ref) => {
           gap-3
         `)}>
         {messages.map((message, index) => (
-          <ChatMessage key={index} message={message} side={index % 2 === 0 ? 'left' : 'right'} />
+          <ChatMessage key={index} message={message} side={message.role === 'user' ? 'right' : 'left'} />
         ))}
       </div>
-      <div className="flex gap-2 items-center absolute bottom-4 left-1/2 -translate-x-1/2 w-[95%]">
-        <Input value={chatInput} onChange={handleChatInput} placeholder="Type a message" />
-        <Button variant="outline" size="icon" className="bg-blue-500 hover:bg-blue-700">
-          <PaperPlaneIcon className="text-white" />
-        </Button>
-      </div>
+      <form onSubmit={handleSubmit} className="absolute bottom-5 left-1/2 -translate-x-1/2 w-full">
+        <div className="flex gap-3 items-center px-4">
+          <Input ref={inputRef} value={chatInput} onChange={handleChatInput} placeholder="Type a message" />
+          <Button type="submit" variant="outline" size="icon" className="size-10 bg-blue-500 hover:bg-blue-700 active:bg-blue-800">
+            <PaperPlaneIcon className="text-white translate-x-[1px]" />
+          </Button>
+        </div>
+      </form>
     </div>
   );
 });
